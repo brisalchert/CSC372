db5cd7d855fbfa5b908ce3250609263a8dd3f3bb
Brian Salchert <brisalchert@gmail.com>
2024-04-17 11:46:07 -0400

    Created Employee.java with constructor

---
+++ src/Module1/Employee.java
@@ -0,0 +1,15 @@
+package Module1;
+
+public class Employee {
+    protected String firstName;
+    protected String lastName;
+    protected int employeeID;
+    protected double salary;
+
+    /**
+     * Constructor: Sets initial salary to $0.00
+     */
+    public Employee() {
+        salary = 0.0;
+    }
+}
