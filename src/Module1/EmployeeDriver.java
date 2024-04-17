5a21026e8b4e6d9941d242e9eaaa395ec7af6a80
Brian Salchert <brisalchert@gmail.com>
2024-04-17 12:14:36 -0400

    Added header comment to driver class

--- src/Module1/EmployeeDriver.java
+++ src/Module1/EmployeeDriver.java
@@ -1,3 +1,29 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  Option #2:  Implementing a Superclass Employee
+//  Part 1: Implement a superclass Employee that has the following fields and methods.
+//
+//    Fields:
+//      string firstName
+//      string lastName
+//      int employeeID
+//      double salary
+//
+//    Methods:
+//      constructor method -   initialize salary field to zero.
+//      setters and getters for firstName, lastName, and employeeID
+//      employeeSummary method - prints all account attributes
+//
+//  Part 2: Implement a Manager class that inherits from the Employee class.
+//
+//    Fields:
+//      string department
+//
+//    Methods:
+//      employeeSummary method - prints all superclass and subclass attributes
+//
+//  Ensure that your program has the two required classes and a test class.
+//----------------------------------------------------------------------------------------------------------------------
+
 package Module1;
 
 public class EmployeeDriver {
