733de583adb5ab4b9f0a4af9ba343eddc054d6b5
Brian Salchert <brisalchert@gmail.com>
2024-04-17 11:48:08 -0400

    Added getters and setters for fields

--- src/Module1/Employee.java
+++ src/Module1/Employee.java
@@ -12,4 +12,36 @@ public class Employee {
     public Employee() {
         salary = 0.0;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
