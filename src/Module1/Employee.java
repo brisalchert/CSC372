25c454659c2a558dff0371aa89dd1e9b10073f5e
Brian Salchert <brisalchert@gmail.com>
2024-04-17 12:03:54 -0400

    Added Javadoc method comments

--- src/Module1/Employee.java
+++ src/Module1/Employee.java
@@ -13,38 +13,73 @@ public class Employee {
         salary = 0.0;
     }
 
+    /**
+     * Gets the first name of the employee
+     * @return the firstName field
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Sets the first name of the employee
+     * @param firstName the String to assign to the field firstName
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Gets the last name of the employee
+     * @return the lastName field
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Sets the last name of the employee
+     * @param lastName the String to assign to the field lastName
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Gets the ID of the employee
+     * @return the employeeID field
+     */
     public int getEmployeeID() {
         return employeeID;
     }
 
+    /**
+     * Sets the ID of the employee
+     * @param employeeID the int to assign to the field employeeID
+     */
     public void setEmployeeID(int employeeID) {
         this.employeeID = employeeID;
     }
 
+    /**
+     * Gets the salary of the employee
+     * @return the salary field
+     */
     public double getSalary() {
         return salary;
     }
 
+    /**
+     * Sets the salary of the employee
+     * @param salary the double to assign to the field salary
+     */
     public void setSalary(double salary) {
         this.salary = salary;
     }
 
+    /**
+     * Prints a summary of all employee info
+     */
     public void employeeSummary() {
         System.out.println("Info for employee with ID: " + employeeID);
         System.out.println("---------------------------------------------");
--- src/Module1/Manager.java
+++ src/Module1/Manager.java
@@ -10,14 +10,25 @@ public class Manager extends Employee {
         super();
     }
 
+    /**
+     * Gets the department of the manager
+     * @return the department field
+     */
     public String getDepartment() {
         return department;
     }
 
+    /**
+     * Sets the department of the manager
+     * @param department the String to assign to the field department
+     */
     public void setDepartment(String department) {
         this.department = department;
     }
 
+    /**
+     * Prints all manager info, calling the superclass method and adding manager info
+     */
     @Override
     public void employeeSummary() {
         super.employeeSummary();
