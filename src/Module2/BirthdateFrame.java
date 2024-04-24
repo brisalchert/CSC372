34067067092fa5a5175daf378a675e87e9afcee4
Brian Salchert <brisalchert@gmail.com>
2024-04-24 11:54:25 -0400

    Added age label and textfield

--- src/Module2/BirthdateFrame.java
+++ src/Module2/BirthdateFrame.java
@@ -8,7 +8,9 @@ import java.awt.event.ActionListener;
 public class BirthdateFrame extends JFrame implements ActionListener {
     private JPanel birthdatePanel;
     private JLabel birthdateLabel;
+    private JLabel ageLabel;
     private JTextField birthdateField;
+    private JTextField ageField;
     private JButton calculateAgeButton;
     private GridBagConstraints layoutConstraints;
 
@@ -19,9 +21,17 @@ public class BirthdateFrame extends JFrame implements ActionListener {
         // Create a label for the birthdate field
         birthdateLabel = new JLabel("Enter your date of birth:");
 
+        // Create a label for the calculated age
+        ageLabel = new JLabel("Age:");
+
         // Create a text field for the user's date of birth
         birthdateField = new JTextField();
         birthdateField.setColumns(10);
+
+        // Create a text field for the user's age
+        ageField = new JTextField();
+        ageField.setColumns(10);
+        ageField.setEditable(false);
 
         // Create a button for calculating the user's age
         calculateAgeButton = new JButton("Calculate Age");
@@ -36,7 +46,6 @@ public class BirthdateFrame extends JFrame implements ActionListener {
         layoutConstraints.gridx = 0;
         layoutConstraints.gridy = 0;
         layoutConstraints.fill = GridBagConstraints.HORIZONTAL;
-        layoutConstraints.anchor = GridBagConstraints.LINE_END;
         layoutConstraints.insets = new Insets(10, 10, 10, 10);
         birthdatePanel.add(birthdateLabel, layoutConstraints);
 
@@ -46,6 +55,21 @@ public class BirthdateFrame extends JFrame implements ActionListener {
         layoutConstraints.gridy = 0;
         layoutConstraints.insets = new Insets(10, 10, 10, 10);
         birthdatePanel.add(birthdateField, layoutConstraints);
+
+        // Create layout constraints for ageLabel
+        layoutConstraints = new GridBagConstraints();
+        layoutConstraints.gridx = 0;
+        layoutConstraints.gridy = 1;
+        layoutConstraints.anchor = GridBagConstraints.LINE_END;
+        layoutConstraints.insets = new Insets(10, 10, 10, 10);
+        birthdatePanel.add(ageLabel, layoutConstraints);
+
+        // Create layout constraints for ageField
+        layoutConstraints = new GridBagConstraints();
+        layoutConstraints.gridx = 1;
+        layoutConstraints.gridy = 1;
+        layoutConstraints.insets = new Insets(10, 10, 10, 10);
+        birthdatePanel.add(ageField, layoutConstraints);
 
         // Add the panel to the frame and pack
         this.add(birthdatePanel);
