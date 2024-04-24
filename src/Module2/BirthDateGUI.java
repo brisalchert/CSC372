05782766d6a5640c892000a0c539840d62a14513
Brian Salchert <brisalchert@gmail.com>
2024-04-24 11:35:12 -0400

    Created birth date GUI classes

---
+++ src/Module2/BirthDateGUI.java
@@ -0,0 +1,19 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  Option #2:  Creating a GUI Application
+//  Create a simple GUI application that will obtain the birthdate from a user. The program must then calculate and
+//  display the age of the user within a JPanel when the user selects a button. The GUI should display the value
+//  presented.
+//
+//  Ensure that your application includes the following components:
+//  - JPanel
+//  - JButton
+//  - ActionListener
+//----------------------------------------------------------------------------------------------------------------------
+
+package Module2;
+
+public class BirthDateGUI {
+    public static void main(String[] args) {
+        
+    }
+}
