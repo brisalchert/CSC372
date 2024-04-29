6cc28f6430f5c49340923807e1e51e09a83e701d
Brian Salchert <brisalchert@gmail.com>
2024-04-29 10:24:51 -0400

    Added CardLayoutFrame and LayoutManagerExample

---
+++ src/Module3/CardLayoutFrame.java
@@ -0,0 +1,14 @@
+package Module3;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CardLayoutFrame extends JFrame implements ActionListener {
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
---
+++ src/Module3/LayoutManagerExample.java
@@ -0,0 +1,12 @@
+package Module3;
+
+import javax.swing.*;
+
+public class LayoutManagerExample {
+    public static void main(String[] args) {
+        CardLayoutFrame frame = new CardLayoutFrame();
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
