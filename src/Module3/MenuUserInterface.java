8d99727da15cd5221411e8a953d3e14426627f12
Brian Salchert <brisalchert@gmail.com>
2024-05-01 12:14:02 -0400

    Added top bar menu

--- src/Module3/MenuUserInterface.java
+++ src/Module3/MenuUserInterface.java
@@ -1,16 +1,36 @@
 package Module3;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MenuUserInterface extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
         // Set title and dimensions of stage
         stage.setTitle("User Interface with Menu");
-        stage.setWidth(400);
-        stage.setHeight(200);
+        stage.setWidth(600);
+        stage.setHeight(400);
 
+        // Create a BorderPane to store UI components
+        BorderPane borderPane = new BorderPane();
+
+        // Create a Menu for the top of the BorderPane
+        Menu menu = new Menu("Menu");
+
+        // Create a MenuBar to hold the Menu
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().add(menu);
+        borderPane.setTop(menuBar);
+
+        // Create a Scene for the BorderPane
+        Scene scene = new Scene(borderPane);
+
+        // Set the scene and show the stage
+        stage.setScene(scene);
         stage.show();
     }
 
