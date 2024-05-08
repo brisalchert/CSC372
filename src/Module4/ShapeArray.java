e4821bec50823027cf7f52766f50eeb8adbad10c
Brian Salchert <brisalchert@gmail.com>
2024-05-08 12:14:49 -0400

    Created ShapeArray.java

---
+++ src/Module4/ShapeArray.java
@@ -0,0 +1,20 @@
+package Module4;
+
+public class ShapeArray {
+    public static void main(String[] args) {
+        Sphere sphere = new Sphere(4.0);
+        Cylinder cylinder = new Cylinder(3.5, 7.5);
+        Cone cone = new Cone(2.0, 10.2);
+        Shape[] shapeArray = {sphere, cylinder, cone};
+
+        // Print header text
+        System.out.println("SHAPE INFO:");
+        System.out.println("--------------------------------------------------");
+
+        // Print information for each shape
+        for (Shape shape : shapeArray) {
+            System.out.println(shape.toString());
+            System.out.println();
+        }
+    }
+}
