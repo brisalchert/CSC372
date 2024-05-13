cf82726d45b432118cef851b9bee425c15f5e62c
Brian Salchert <brisalchert@gmail.com>
2024-05-13 11:25:16 -0400

    Created RecursiveProduct.java

---
+++ src/Module5/RecursiveProduct.java
@@ -0,0 +1,15 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  Option #1:  Implementing Recursion to Provide a Product
+//  ---------------------------------------------------------
+//
+//  Using recursion, create a program that will allow a user to enter five numbers. The program will provide the
+//  product of all five numbers using recursive methods.
+//----------------------------------------------------------------------------------------------------------------------
+
+package Module5;
+
+public class RecursiveProduct {
+    public static void main(String[] args) {
+
+    }
+}
