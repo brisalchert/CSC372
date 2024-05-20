f04c0d8f80634cdf8dfe3690b3790a373733d1d6
Brian Salchert <brisalchert@gmail.com>
2024-05-20 11:46:52 -0400

    Added output with students sorted different ways

--- src/Module6/StudentSorting.java
+++ src/Module6/StudentSorting.java
@@ -1,3 +1,16 @@
+//----------------------------------------------------------------------------------------------------------------------
+// Option #2: Storing an ArrayList II
+//  Create a Java program that will store 10 student objects in an ArrayList, ArrayList<Student>. A student object
+//  consists of the following fields:
+//
+//  - int rollno
+//  - String name
+//  - String address
+//
+//  Implement a comparator class to sort student objects by rollno (roll number). Implement your own merge sort method
+//  and place your code in a separate Java source file. Do not use a sort method from the Java collections library.
+//----------------------------------------------------------------------------------------------------------------------
+
 package Module6;
 
 import java.util.ArrayList;
@@ -26,6 +39,36 @@ public class StudentSorting {
         // Create 10 new Student objects and add them to the ArrayList
         for (int index = 0; index < 10; index++) {
             students.add(new Student(rollnoArray[index], nameArray[index], AddressArray[index]));
+        }
+
+        // Print the initial list of Students:
+        System.out.println("INITIAL LIST OF STUDENTS");
+        System.out.println("------------------------------------------------------");
+
+        for (Student student : students) {
+            System.out.println(student.toString());
+        }
+
+        System.out.println();
+
+        // Print the list of Students sorted by roll number
+        mergeSort.mergeSort(students, new RollnoComparator());
+        System.out.println("LIST OF STUDENTS SORTED BY ROLL NUMBER");
+        System.out.println("------------------------------------------------------");
+
+        for (Student student : students) {
+            System.out.println(student.toString());
+        }
+
+        System.out.println();
+
+        // Print the list of Students sorted by name
+        mergeSort.mergeSort(students, new NameComparator());
+        System.out.println("LIST OF STUDENTS SORTED BY NAME");
+        System.out.println("------------------------------------------------------");
+
+        for (Student student : students) {
+            System.out.println(student.toString());
         }
     }
 }
