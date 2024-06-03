06ca66f3d0b0fd563cefc8741416cab995ae7b10
Brian Salchert <brisalchert@gmail.com>
2024-06-03 12:40:14 -0400

    Added input processing for Student data

--- src/Module8/StudentData.java
+++ src/Module8/StudentData.java
@@ -15,13 +15,54 @@
 
 package Module8;
 
+import java.util.InputMismatchException;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class StudentData {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         // Create a LinkedList to store Students
         LinkedList<Student> studentList = new LinkedList<Student>();
 
-        
+        // Get Student data from the user until they choose to exit
+        System.out.print("Enter data for a new student? (Y/N): ");
+        String userChoice = input.nextLine().toLowerCase();
+
+        while (!userChoice.equals("n")) {
+            if (userChoice.equals("y")) {
+                String name;
+                String address;
+                double gpa;
+
+                // Get the Student's name
+                System.out.print("Enter student name: ");
+                name = input.nextLine();
+
+                // Get the Student's address
+                System.out.print("Enter student address: ");
+                address = input.nextLine();
+
+                // Get the Student's gpa, validating the input and requesting again on invalid input
+                System.out.print("Enter student GPA: ");
+                try {
+                    gpa = input.nextDouble();
+                }
+                catch (InputMismatchException e) {
+                    System.out.println("Invalid input -- GPA must be a number.");
+                }
+                finally {
+                    // Clear the input for the next iteration of the loop
+                    input.nextLine();
+                }
+            }
+            else {
+                System.out.println("Invalid input -- please enter Y or N.");
+            }
+
+            // Ask if the user wishes to continue processing input
+            System.out.print("Enter data for a new student? (Y/N): ");
+            userChoice = input.nextLine().toLowerCase();
+        }
     }
 }
