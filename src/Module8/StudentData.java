cc640f4ebd6109d5473c0b6f5a2a797f9b21737d
Brian Salchert <brisalchert@gmail.com>
2024-06-03 12:25:50 -0400

    Created StudentData.java

---
+++ src/Module8/StudentData.java
@@ -0,0 +1,27 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  Write a Java program that incorporates a loop that prompts the user for student data. Student data are private
+//  fields in a student class including:
+//
+//  - String name
+//  - String address
+//  - double GPA
+//
+//  Each student object is stored in a linked list. After the user completes the data entry, output the contents of the
+//  linked list in ascending sorted order by name to a regular text file that can be opened and viewed using a simple
+//  plain-text editor such as notepad.
+//
+//  Validate numeric data for Grade Point Average (GPA).
+//----------------------------------------------------------------------------------------------------------------------
+
+package Module8;
+
+import java.util.LinkedList;
+
+public class StudentData {
+    public static void main(String[] args) {
+        // Create a LinkedList to store Students
+        LinkedList<Student> studentList = new LinkedList<Student>();
+
+        
+    }
+}
