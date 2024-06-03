f476d5b0b23211d7936fbdcaf1bd8770efa321bf
Brian Salchert <brisalchert@gmail.com>
2024-06-03 12:16:55 -0400

    Created Student.java for Portfolio Project

---
+++ src/Module8/Student.java
@@ -0,0 +1,13 @@
+package Module8;
+
+public class Student {
+    private String name;
+    private String address;
+    private double gpa;
+
+    public Student(String name, String address, double gpa) {
+        this.name = name;
+        this.address = address;
+        this.gpa = gpa;
+    }
+}
